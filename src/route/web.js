23b83b32f9c6ecad9d64d58a073ff390a8957851
dalaryizmi99 <vang.vs.59cntt@ntu.edu.vn>
2023-07-31 02:32:53 +0700

    completed CURD

--- src/route/web.js
+++ src/route/web.js
@@ -5,6 +5,13 @@ let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get('/', homeController.getHomePage);
+    router.get('/create-crud', homeController.getCRUD);
+
+    router.post('/post-crud', homeController.postCRUD);
+    router.get('/display-user-crud', homeController.displayCRUD);
+    router.get('/edit-crud', homeController.getEditCRUD);
+    router.post('/put-update-info-crud', homeController.putUpdateInfoCRUD);
+    router.get('/delete-user-crud', homeController.deleteUserCRUD);
 
     return app.use("/", router);
 }
