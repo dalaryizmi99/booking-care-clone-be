70905fca8dad4564caacf0c38500649a915ea59c
dalaryizmi99 <vang.vs.59cntt@ntu.edu.vn>
2023-11-03 02:22:40 +0700

    create get detail specialty api and service

--- src/services/specialtyService.js
+++ src/services/specialtyService.js
@@ -45,12 +45,65 @@ let getAllSpecialty = (data) => {
                 data
             })
         } catch (e) {
-            reject(e)
+            reject(e);
+        }
+    })
+}
+
+let getDetailSpecialtyById = (inputId, location) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputId || !location) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing parameters!'
+                })
+            } else {
+                let data = await db.Specialty.findOne({
+                    where: {
+                        id: inputId
+                    },
+                    attributes: ['descriptionHTML', 'descriptionMarkdown'],
+                })
+
+                if (data) {
+                    let doctorSpecialty = [];
+                    if (location === 'ALL') {
+                        doctorSpecialty = await db.Doctor_Infor.findAll({
+                            where: {
+                                specialtyId: inputId
+                            },
+                            attributes: ['doctorId', 'provinceId']
+                        })
+                    } else {
+                        //find doctor of specialty by location
+                        doctorSpecialty = await db.Doctor_Infor.findAll({
+                            where: {
+                                specialtyId: inputId,
+                                provinceId: location
+                            },
+                            attributes: ['doctorId', 'provinceId']
+                        })
+                    }
+
+                    data.doctorSpecialty = doctorSpecialty;
+                } else data = {}
+
+                resolve({
+                    message: 'success!',
+                    errCode: 0,
+                    data
+                })
+
+            }
+        } catch (e) {
+            reject(e);
         }
     })
 }
 
 module.exports = {
     createSpecialty: createSpecialty,
-    getAllSpecialty: getAllSpecialty
+    getAllSpecialty: getAllSpecialty,
+    getDetailSpecialtyById: getDetailSpecialtyById
 }
